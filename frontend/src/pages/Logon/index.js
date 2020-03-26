bbebd41a548a34934d689e7d37babeed78834751
jfbatista <jfbatista@hotmail.com>
2020-03-26 18:44:25 -0300

    Finalizado FrontEnd

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+            history.push('/profile');
+        } catch (error) {
+            alert(`Falha no login: ${error}`);
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="logo" />
 
-                <form action="">
+                <form action="" onSubmit={handleLogin}>
                     <h1>Faça seu Login</h1>
 
-                    <input type="text" placeholder="Sua ID" />
+                    <input
+                        type="text"
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+
                     <button className="button">Entrar</button>
 
                     <Link className="back-link" to="/register">
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,41 +1,81 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi';
 
+import api from '../../services/api';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+    const [title, setTitle] = useState();
+    const [description, setDescription] = useState();
+    const [value, setValue] = useState();
+    const ongId = localStorage.getItem('ongId');
+    const history = useHistory();
+
+    async function handleIncident(e) {
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        }
+
+        console.log(value)
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId
+                }
+            });
+
+            history.push('/profile');
+
+        } catch (error) {
+            alert(`Erro no cadastro: ${error}`);
+        }
+    }
+
     return (
-        <div className="profile-container">
+        <div className="incident-container">
             <div className="content">
                 <section>
                     <img src={logoImg} alt="Logo" />
 
                     <h1>Cadastro</h1>
-                    <p>Faça aqui seu cadastro. Ajude uma ONG a salvar um animal</p>
+                    <p>Faça aqui o cadastro do seu caso.</p>
 
-                    <Link className="back-link" to="/">
+                    <Link className="back-link" to="/profile">
                         <FiArrowLeft size={16} color="#E02041" />
                         Voltar
                     </Link>
                 </section>
 
-                <form action="">
-                    <input placeholder="Nome da ONG" />
-                    <input type="email" placeholder="email" />
-                    <input placeholder="WhatsApp" />
+                <form action="" onSubmit={handleIncident}>
+                    <input
+                        placeholder="Titulo"
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
 
-                    <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={{ width: 80 }} />
-                    </div>
+                    <textarea
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+
+                    <input
+                        placeholder="Valor"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
 
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
 
-
-        </div>
+        </div >
     )
 }
